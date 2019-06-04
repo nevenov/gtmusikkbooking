53ab1a91a3b6253162a4259ad9b92e9c808c99cb
Stoyan Nedev <nevenov@abv.bg>
2019-06-04 17:37:02 +0300

    create artist page almost read - need text editor and Photo model

--- resources/views/admin/artists/create.blade.php
+++ resources/views/admin/artists/create.blade.php
@@ -13,7 +13,7 @@
                                 <h3 class="mb-0">{{ __('Добави артист') }}</h3>
                             </div>
                             <div class="col-4 text-right">
-                                <a href="{{ route('admin.artists.index') }}" class="btn btn-sm btn-primary">{{ __('Обратно') }}</a>
+                                <a href="{{ route('admin.artists.index') }}" class="btn btn-sm btn-primary">{{ __('Всички артисти') }}</a>
                             </div>
                         </div>
                     </div>
