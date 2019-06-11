b87340fce64fc28c986e626719772a561dbcedbc
Stoyan Nedev <nevenov@abv.bg>
2019-06-11 17:30:45 +0300

    Front-end css cleaned, index blade ready

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,133 @@
 @extends('layouts.front')
+
+
+
+@section('content')
+
+    <section id="about-us" class="white">
+        <div class="container">
+            <div class="gap"></div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="center gap fade-down section-heading">
+                        <h2 class="main-title">~ Om oss ~</h2>
+                        <hr>
+                        <p>We started our business 20 years ago.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-10 col-md-offset-1 fade-up">
+                    <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+
+                    <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
+                </div>
+                <div class="col-md-4 fade-up">
+
+                </div>
+            </div>
+            <div class="gap"></div>
+
+            <div class="gap"></div>
+
+            <div class="center gap fade-down section-heading">
+                <h2 class="main-title">~ Artister ~</h2>
+                <hr>
+                <p>Velg ønsket artister kategori:</p>
+            </div>
+            <div class="gap"></div>
+
+            <div id="artists" class="row purewhite">
+                <div class="col-md-3 col-xs-6">
+                    <div class="center team-member">
+                        <div class="team-image">
+                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
+                            <div class="overlay">
+                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
+                            </div>
+                        </div>
+                        <div class="team-content fade-up">
+                            <h5><a href="">Bryllup</a>
+                                <small class="role muted"><br><a href="">Se artister</a></small>
+                            </h5>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-3 col-xs-6 mb-4">
+                    <div class="center team-member">
+                        <div class="team-image">
+                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
+                            <div class="overlay">
+                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
+                            </div>
+                        </div>
+                        <div class="team-content fade-up">
+                            <h5><a href="">Bryllup</a>
+                                <small class="role muted"><br><a href="">View Artists</a></small>
+                            </h5>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-xs-6">
+                    <div class="center team-member">
+                        <div class="team-image">
+                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
+                            <div class="overlay">
+                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
+                            </div>
+                        </div>
+                        <div class="team-content fade-up">
+                            <h5><a href="">Bryllup</a>
+                                <small class="role muted"><br><a href="">View Artists</a></small>
+                            </h5>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-xs-6">
+                    <div class="center team-member">
+                        <div class="team-image">
+                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
+                            <div class="overlay">
+                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
+                            </div>
+                        </div>
+                        <div class="team-content fade-up">
+                            <h5><a href="">Bryllup</a>
+                                <small class="role muted"><br><a href="">View Artists</a></small>
+                            </h5>
+
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-3 col-xs-6">
+                    <div class="center team-member">
+                        <div class="team-image">
+                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
+                            <div class="overlay">
+                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
+                            </div>
+                        </div>
+                        <div class="team-content fade-up">
+                            <h5><a href="">Bryllup</a>
+                                <small class="role muted"><br><a href="">View Artists</a></small>
+                            </h5>
+
+                        </div>
+                    </div>
+                </div>
+            </div><!--/#artists-->
+
+            <!--- <div class="gap"></div> --->
+        </div>
+    </section>
+
+@endsection
--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -44,25 +44,14 @@
                 <span class="sr-only">Toggle navigation</span>
                 <i class="fa fa-bars"></i>
             </button>
-            <a class="navbar-brand" href="index.html"><h1>G.T.MUSIKKBOOKING</h1></a>
+            <a class="navbar-brand" href="{{ url('/') }}"><h1>G.T.MUSIKKBOOKING</h1></a>
         </div>
         <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about-us.html">About Us</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="portfolio.html">Portfolio</a></li>
-                <li><a href="blog.html">Blog</a></li>
-                <li><a href="contact-us.html">Contact</a></li>
-                <li class="dropdown active">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Pages <i class="icon-angle-down"></i></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="project-item.html">Project Single</a></li>
-                        <li><a href="blog-item.html">Blog Single</a></li>
-                        <li class="active"><a href="404.html">404</a></li>
-                    </ul>
-                </li>
-                <!--- <li><span class="search-trigger"><i class="fa fa-search"></i></span></li> --->
+                <li><a href="{{ url('/') }}#single-page-slider">Hjem</a></li>
+                <li><a href="{{ url('/') }}#about-us">Om oss</a></li>
+                <li><a href="{{ url('/') }}#artists">Artister</a></li>
+                <li><a href="{{ url('/') }}#contact">Kontakt oss</a></li>
             </ul>
         </div>
     </div>
@@ -77,8 +66,8 @@
                         <div class="col-sm-12">
                             <div class="carousel-content center centered">
                                 <span class="home-icon pe-7s-gleam bounce-in"></span>
-                                <h2 class="boxed animation animated-item-1 fade-down">WELCOME TO G.T.MUSIKKBOOKING ARTIST AGENCY</h2>
-                                <p class="boxed animation animated-item-2 fade-up">Our experts will find the best musician for you to success</p>
+                                <h2 class="boxed animation animated-item-1 fade-down">VELKOMMEN TIL G.T.MUSIKKBOOKING ARTISTER AGENTUR</h2>
+                                <p class="boxed animation animated-item-2 fade-up">Våre eksperter vil finne den beste musikken for deg til suksess.</p>
                             </div>
                         </div>
                     </div>
@@ -90,144 +79,21 @@
 
 <div id="content-wrapper">
 
-    <section id="about-us" class="white">
-        <div class="container">
-            <div class="gap"></div>
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="center gap fade-down section-heading">
-                        <h2 class="main-title">~ About Us ~</h2>
-                        <hr>
-                        <p>Of an or game gate west face shed. ﻿no great but music too old found arose.</p>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-md-10 col-md-offset-1 fade-up">
-                    <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
 
-                    <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non  mauris vitae erat consequat auctor eu in elit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris in erat justo. Nullam ac urna eu felis dapibus condimentum sit amet a augue. Sed non neque elit. Sed ut imperdiet nisi. Proin condimentum fermentum nunc. Etiam pharetra, erat sed fermentum feugiat, velit mauris egestas quam, ut aliquam massa nisl quis neque. Suspendisse in orci enim.</p>
-                </div>
-                <div class="col-md-4 fade-up">
-
-                </div>
-            </div>
-            <div class="gap"></div>
-
-            <div class="gap"></div>
-
-            <div class="center gap fade-down section-heading">
-                <h2 class="main-title">ARTISTS</h2>
-                <hr>
-                <p>Choose the desired artists category:</p>
-            </div>
-            <div class="gap"></div>
-
-            <div id="meet-the-team" class="row purewhite">
-                <div class="col-md-3 col-xs-6">
-                    <div class="center team-member">
-                        <div class="team-image">
-                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
-                            <div class="overlay">
-                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="team-content fade-up">
-                            <h5><a href="">Bryllup</a>
-                                <small class="role muted"><br><a href="">View Artists</a></small>
-                            </h5>
-
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="col-md-3 col-xs-6 mb-4">
-                    <div class="center team-member">
-                        <div class="team-image">
-                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
-                            <div class="overlay">
-                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="team-content fade-up">
-                            <h5><a href="">Bryllup</a>
-                                <small class="role muted"><br><a href="">View Artists</a></small>
-                            </h5>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3 col-xs-6">
-                    <div class="center team-member">
-                        <div class="team-image">
-                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
-                            <div class="overlay">
-                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="team-content fade-up">
-                            <h5><a href="">Bryllup</a>
-                                <small class="role muted"><br><a href="">View Artists</a></small>
-                            </h5>
-
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-md-3 col-xs-6">
-                    <div class="center team-member">
-                        <div class="team-image">
-                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
-                            <div class="overlay">
-                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="team-content fade-up">
-                            <h5><a href="">Bryllup</a>
-                                <small class="role muted"><br><a href="">View Artists</a></small>
-                            </h5>
-
-                        </div>
-                    </div>
-                </div>
-
-
-                <div class="col-md-3 col-xs-6">
-                    <div class="center team-member">
-                        <div class="team-image">
-                            <img class="img-responsive img-thumbnail bounce-in" src="{{ asset('front') }}/images/bg/groups-pop-rock.jpg" alt="">
-                            <div class="overlay">
-                                <a class="preview btn btn-outlined btn-primary" href=""><i class="fa fa-link"></i></a>
-                            </div>
-                        </div>
-                        <div class="team-content fade-up">
-                            <h5><a href="">Bryllup</a>
-                                <small class="role muted"><br><a href="">View Artists</a></small>
-                            </h5>
-
-                        </div>
-                    </div>
-                </div>
-            </div><!--/#meet-the-team-->
-
-            <!--- <div class="gap"></div> --->
-        </div>
-    </section>
+    @yield('content')
 
     <section id="contact" class="white">
         <div class="container">
             <!--- <div class="gap"></div> --->
             <div class="center gap fade-down section-heading">
-                <h2 class="main-title">Get In Touch</h2>
+                <h2 class="main-title">~ Ta kontakt ~</h2>
                 <hr>
-                <p>Of an or game gate west face shed. ﻿no great but music too old found arose.</p>
+                <p>Send forespørselen din her. Vi vil svare så snart som mulig.</p>
             </div>
             <div class="gap"></div>
             <div class="row">
                 <div class="col-md-4 fade-up">
-                    <h3>Contact Information</h3>
+                    <h3>Kontakt</h3>
                     <p><span class="icon icon-home"></span>Time Square, New York<br/>
                         <span class="icon icon-phone"></span>+36 65984 405<br/>
                         <span class="icon icon-mobile"></span>+36 65984 405<br/>
@@ -238,7 +104,7 @@
                 </div><!-- col -->
 
                 <div class="col-md-8 fade-up">
-                    <h3>Drop Us A Message</h3>
+                    <h3>Send oss en melding</h3>
                     <br>
                     <br>
                     <div id="message"></div>
@@ -265,50 +131,31 @@
                     <p>Was drawing natural fat respect husband. An as noisy an offer drawn blush place. These tried for way joy wrote witty. In mr began music weeks after at begin.</p>
                 </div><!--/.col-md-3-->
 
-                <div class="col-md-3 col-sm-6">
-                    <h4>Company</h4>
-                    <div>
-                        <ul class="arrow">
-                            <li><a href="#">Company Overview</a></li>
-                            <li><a href="#">Meet The Team</a></li>
-                            <li><a href="#">Our Awesome Partners</a></li>
-                            <li><a href="#">Our Services</a></li>
-                        </ul>
-                    </div>
-                </div><!--/.col-md-3-->
+                <div class="col-md-2"></div>
 
                 <div class="col-md-3 col-sm-6">
-                    <h4>Latest Articles</h4>
-                    <div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <img class="widget-img" src="images/portfolio/folio01.jpg" alt="">
-                            </div>
-                            <div class="media-body">
-                                <span class="media-heading"><a href="#">Blog Post A</a></span>
-                                <small class="muted">Posted 14 April 2014</small>
-                            </div>
-                        </div>
-                        <div class="media">
-                            <div class="pull-left">
-                                <img class="widget-img" src="images/portfolio/folio02.jpg" alt="">
-                            </div>
-                            <div class="media-body">
-                                <span class="media-heading"><a href="#">Blog Post B</a></span>
-                                <small class="muted">Posted 14 April 2014</small>
-                            </div>
-                        </div>
-                    </div>
-                </div><!--/.col-md-3-->
-
-                <div class="col-md-3 col-sm-6">
-                    <h4>Come See Us</h4>
+                    <h4>Kontakt oss</h4>
                     <address>
                         <strong>Ace Towers</strong><br>
                         New York Ave,<br>
                         New York, 215648<br>
                         <abbr title="Phone"><i class="fa fa-phone"></i></abbr> 546 840654 05
                     </address>
+
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-1"></div>
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Partnere</h4>
+                    <div>
+                        <ul class="arrow">
+                            <li><a href="#">Website 1</a></li>
+                            <li><a href="#">Website 2</a></li>
+                            <li><a href="#">Website 3</a></li>
+                            <li><a href="#">Website 4</a></li>
+                        </ul>
+                    </div>
                 </div> <!--/.col-md-3-->
             </div>
         </div>
@@ -318,7 +165,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-8">
-                    &copy; 2019 Your Site Name. All Rights Reserved. <a href="https://templatemag.com/bootstrap-templates/">Bootstrap templates</a> by TemplateMag.
+                    &copy; {{ date('Y') }} <a href="{{ url('/') }}">GTMUSIKKBOOKING.COM</a>. Alle rettigheter reservert.
                 </div>
                 <div class="col-sm-4">
                     <ul class="pull-right">
@@ -334,7 +181,6 @@
 <script src="{{ asset('front') }}/js/plugins.js"></script>
 <script src="{{ asset('front') }}/js/bootstrap.min.js"></script>
 <script src="{{ asset('front') }}/js/jquery.prettyPhoto.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCWDPCiH080dNCTYC-uprmLOn2mt2BMSUk&amp;sensor=true"></script>
 <script src="{{ asset('front') }}/js/init.js"></script>
 </body>
 </html>
